aa08e1f2d4a000bd36d31d0b0eb7cb594a814fb1
NooobtimeX <u2gstudio@gmail.com>
2023-11-29 09:59:38 +0700

    Create page.tsx

---
+++ app/acrylic/page.tsx
@@ -0,0 +1,13 @@
+export default function Acrylic() {
+  return (
+    <div>
+      <h1 className="text-center">โล่รางวัลอะคริลิค</h1>
+      <div>
+        <h2 className="text-center">โล่รางวัลอะคริลิค</h2>
+      </div>
+      <div>
+        <h2 className="text-center">โล่รางวัลอะคริลิค</h2>
+      </div>
+    </div>
+  );
+}
