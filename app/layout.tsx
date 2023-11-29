28dc9d251b8321fc209bcc514f7c2974121f922d
NooobtimeX <u2gstudio@gmail.com>
2023-11-29 10:45:44 +0700

    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 import AppHeader from "@/components/AppHeader";
 import AppFooter from "@/components/AppFooter";
 import { SocialBar } from "@/components/SocialBar";
-
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,11 +20,44 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Tag Manager */}
+        <Script
+          id="googletagmanager"
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){
+                  w[l]=w[l]||[];
+                  w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                  var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                  j.async=true;
+                  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                  f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-M86WK6ZW');
+              `,
+          }}
+          strategy="lazyOnload"
+        />
+
+        {/* End Google Tag Manager */}
+      </Head>
       <body className={inter.className + " bg-white "}>
         <AppHeader />
         <main className="mx-auto max-w-7xl">{children}</main>
         <SocialBar />
         <AppFooter />
+        <NextScript />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M86WK6ZW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   );
