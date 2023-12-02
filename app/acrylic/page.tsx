1f464f3c44181d692dc713356c4d42a1e176acff
NooobtimeX <u2gstudio@gmail.com>
2023-12-02 11:53:19 +0700

    Update page.tsx

--- app/acrylic/page.tsx
+++ app/acrylic/page.tsx
@@ -1,44 +1,50 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 export const metadata: Metadata = {
   title: "ราคาโล่รางวัลอะคริลิค",
   description: "ราคาโล่รางวัลอะคริลิค",
   keywords:
     "RS AWARD, โล่รางวัล, โล่อะคริลิค, โล่รางวัลอะคริลิค, ราคาโล่รางวัลอะคริลิค",
 };
-let fs = [{ src: "1" }];
-let cs = [{ src: "0" }];
-for (let i = 2; i <= 13; i++) {
-  fs.push({
-    src: i.toString(),
-  });
-}
-for (let i = 1; i <= 13; i++) {
-  cs.push({
-    src: i.toString(),
-  });
-}
+const images = [
+  {
+    name: "โล่อะคริลิคสำเร็จรูป",
+    link: "/acrylic/finished",
+    description: "",
+    src: "/panel/finished_acrylic.webp",
+    alt: "",
+  },
+  {
+    name: "โล่อะคริลิคสั่งทำ",
+    link: "/acrylic/custom",
+    description: "",
+    src: "/panel/custom_acrylic.webp",
+    alt: "",
+  },
+];
 export default function Acrylic() {
   return (
-    <div>
-      <div id="finished">
-        <h1 className="text-center py-2">โล่รางวัลอะคริลิคสำเร็จรูป</h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
-          {fs.map((f, index) => (
-            <div key={index}>
-              <img src={"/catalog/finished_acrylic/" + f.src + ".jpg"} />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div id="custom">
-        <h1 className="text-center">โล่รางวัลอะคริลิคสั่งทำ</h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
-          {cs.map((c, index) => (
-            <div key={index}>
-              <img src={"/catalog/custom_acrylic/" + c.src + ".webp"} />
-            </div>
-          ))}
-        </div>
+    <div className="py-4">
+      <h1 className="text-center">โล่รางวัลอะคริลิค</h1>
+      <div className="grid grid-cols-2 gap-4 sm:gap-6 lg:gap-8 py-2">
+        {images.map((image) => (
+          <div
+            className="relative h-full transition-transform duration-300 hover:scale-105"
+            key={image.name}
+          >
+            <Link href={image.link}>
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="w-full h-auto rounded-lg bg-gray-100 "
+                aria-label=""
+              />
+              <div className="mx-auto text-center">
+                <h2>{image.name}</h2>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
