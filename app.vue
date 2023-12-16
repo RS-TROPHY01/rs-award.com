12bc25c692f5bd43178cc5c20698501878e01d97
NooobtimeX <u2gstudio@gmail.com>
2023-12-16 11:31:40 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <div class="py-4 items-center px-2 mx-auto max-w-6xl">
+    <div class="py-1 items-center px-2 mx-auto max-w-6xl">
       <NuxtPage />
     </div>
   </NuxtLayout>
