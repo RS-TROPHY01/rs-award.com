577f7c564f33e9cd987f5c8988fc9d91ac4f7524
NooobtimeX <u2gstudio@gmail.com>
2023-12-16 09:55:42 +0700

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,17 +4,15 @@
       class="grid grid-cols-2 lg:grid-cols-4 text-white mx-auto text-center p-4 max-w-7xl"
     >
       <div class="text-center mx-auto my-auto col-span-1">
-        <h5 class="text-center p-2 font-bold text-2xl text-white">
-          สินค้าอื่นๆ
-        </h5>
+        <p class="text-center p-2 font-bold text-2xl text-white">สินค้าอื่นๆ</p>
         <div v-for="product in otherProducts" :key="product.name" class="p-1">
-          <a :to="product.href" class="hover:text-blue-500">
+          <a :href="product.href" class="hover:text-blue-500">
             <h6 class="font-bold text-xl text-white">{{ product.name }}</h6>
           </a>
         </div>
       </div>
       <div class="text-center mx-auto my-auto col-span-1">
-        <h5 class="text-center p-2 font-bold text-2xl text-white">เบอร์โทร</h5>
+        <p class="text-center p-2 font-bold text-2xl text-white">เบอร์โทร</p>
         <div class="p-1 text-white">
           <ul>
             <li class="text-white">0649370011</li>
@@ -25,6 +23,7 @@
       </div>
       <div class="col-span-2 rounded-lg w-full h-60 p-4">
         <iframe
+          title="RS AWARD Google Map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3872.01552442906!2d100.54981097587556!3d13.957681286455957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e281423d9ee0cb%3A0xc0c6c349cc115b53!2zUnMgdHJvcGh5IOC4o-C5iOC4p-C4oeC4quC4uOC4guC4luC5ieC4p-C4ouC4o-C4suC4h-C4p-C4seC4pQ!5e0!3m2!1sth!2sth!4v1695441505250!5m2!1sth!2sth"
           loading="lazy"
           class="w-full h-full rounded-lg"
