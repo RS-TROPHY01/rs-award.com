1ec81e0717262dcd8f9151ca9cbfb9cf8036e9b9
NooobtimeX <u2gstudio@gmail.com>
2023-12-16 09:39:53 +0700

    Update AppCategory.vue

--- components/AppCategory.vue
+++ components/AppCategory.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8 py-2">
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8 py-2">
     <div
       class="relative h-full transition-transform duration-300 hover:scale-105"
       v-for="image in images"
