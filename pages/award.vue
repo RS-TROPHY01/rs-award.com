2bd890c493e52e7c3584d82c0aff50d5dd568dc1
NooobtimeX <u2gstudio@gmail.com>
2023-12-16 11:32:10 +0700

    Update award.vue

--- pages/award.vue
+++ pages/award.vue
@@ -16,10 +16,8 @@ useSeoMeta({
 <template>
   <div class="bg-white py-4">
     <div class="mx-auto items-center gap-x-8 gap-y-16 p-2 lg:px-8">
-      <div class="py-4">
-        <h1 class="text-center">โล่รางวัล</h1>
-        <AppCategory />
-      </div>
+      <h1 class="text-center">โล่รางวัล</h1>
+      <AppCategoryForSale />
     </div>
   </div>
 </template>
