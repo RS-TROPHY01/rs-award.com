06a9c5b15a0d7cd895b1b10b471152c97474d8c4
NooobtimeX <u2gstudio@gmail.com>
2023-12-18 09:34:04 +0700

    Update AppHeroSection.vue

--- components/AppHeroSection.vue
+++ components/AppHeroSection.vue
@@ -2,12 +2,9 @@
   <div class="relative h-full">
     <NuxtImg
       src="/panel/hero_section.webp"
-      width="0"
-      height="0"
-      sizes="100vw"
       class="w-full h-auto"
       alt="RS AWARD PANEL"
-      aria-label=""
+      aria-label="RS AWARD PANEL"
     />
   </div>
 </template>
