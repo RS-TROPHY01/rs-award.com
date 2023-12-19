b954f032adeda563052ac2b61894c020a165a5bb
NooobtimeX <u2gstudio@gmail.com>
2023-12-19 10:52:48 +0700

    add AppPassWork

--- components/AppPassWork.vue
+++ components/AppPassWork.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="mx-auto">
     <h2 class="text-center">ผลงานที่ผ่านมา</h2>
+    <div id="pastwork">
+      <img
+        v-for="index in 3"
+        :key="index"
+        :src="`/pastwork/${index}.jpg`"
+        loading="lazy"
+        alt="ผลงานโล่รางวัลที่ผ่านมา"
+        title="ผลงานโล่รางวัลที่ผ่านมา"
+        aria-label="ผลงานโล่รางวัลที่ผ่านมา"
+      />
+    </div>
   </div>
 </template>
+<style>
+#pastwork {
+  gap: 0.5rem /* 8px */;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+}
+</style>
