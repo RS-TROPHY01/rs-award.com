06ecaad79ac7585b5d1bce4787c8aa8a14f5b217
NooobtimeX <u2gstudio@gmail.com>
2023-12-19 13:45:22 +0700

    split

---
+++ components/AppCopyRight.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full max-w-screen-xl mx-auto p-4 md:py-4">
+    <span class="block text-sm sm:text-center text-gray-400">
+      © 2023 <router-link to="/">RS AWARD</router-link>. All Rights Reserved.
+    </span>
+  </div>
+</template>
---
+++ components/AppGoogleMap.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="rounded-lg w-full h-60 p-4">
+    <a
+      aria-label="RS AWARD GOOGLE MAP"
+      href="https://maps.app.goo.gl/U1C7R9aiBJHprLfx8"
+    >
+      <img
+        class="mx-auto"
+        alt="RS AWARD GOOGLE MAP"
+        title="RS AWARD GOOGLE MAP"
+        src="/panel/map.png"
+    /></a>
+  </div>
+</template>
