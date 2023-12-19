20d46cc537968c06b409331ff76f18c38cde05e7
NooobtimeX <u2gstudio@gmail.com>
2023-12-19 09:22:11 +0700

    add AppOurCustomer

---
+++ components/AppOurCustomer.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2 class="text-center p-2">ลูกค้าของเรา</h2>
+  <div class="gap-2 grid grid-cols-5 md:grid-cols-10">
+    <div v-for="(customer, index) in customers" :key="index">
+      <img width="100px" height="100px" :src="customer.src" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      customers: [
+        { name: "KFC", src: "/customer/kfc.png" },
+        { name: "Lazada", src: "/customer/lazada.png" },
+        { name: "Shopee", src: "/customer/shopee.webp" },
+        { name: "SCB", src: "/customer/SCB.webp" },
+        { name: "KBank", src: "/customer/KBank.webp" },
+      ],
+    };
+  },
+};
+</script>
