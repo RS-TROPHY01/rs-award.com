f46241564f9f128aecad84868ea4381d44300e5d
NooobtimeX <u2gstudio@gmail.com>
2023-12-19 16:51:07 +0700

    Update AppOurCustomer.vue

--- components/AppOurCustomer.vue
+++ components/AppOurCustomer.vue
@@ -1,7 +1,11 @@
 <template>
   <h2 class="text-center p-2">ลูกค้าของเรา</h2>
   <div class="gap-2 grid grid-cols-4 md:grid-cols-6">
-    <div v-for="(customer, index) in customers" :key="index" class="m-auto">
+    <div
+      v-for="(customer, index) in customers"
+      :key="index"
+      class="m-auto scaleup"
+    >
       <img
         width="240px"
         class="m-auto"
@@ -18,17 +22,18 @@ export default {
   data() {
     return {
       customers: [
-        { name: "ธนาคารไทยพาณิชย์", src: "/customer/SCB.webp" },
-        { name: "ธนาคารกสิกรไทย", src: "/customer/KBank.webp" },
-        { name: "ธนาคารกรุงเทพ", src: "/customer/BBL.webp" },
-        { name: "ธนาคารออมสิน", src: "/customer/GSB.webp" },
-        { name: "ธนาคารกรุงไทย", src: "/customer/KTB.webp" },
+        { name: "ธนาคารไทยพาณิชย์", src: "/customer/scb.webp" },
+        { name: "ธนาคารกสิกรไทย", src: "/customer/kbank.webp" },
+        { name: "ธนาคารกรุงเทพ", src: "/customer/bbl.webp" },
+        { name: "ธนาคารออมสิน", src: "/customer/gsb.webp" },
+        { name: "ธนาคารกรุงไทย", src: "/customer/ktb.webp" },
         { name: "Lazada", src: "/customer/lazada.webp" },
         { name: "Shopee", src: "/customer/shopee.webp" },
+        { name: "ช่องเวิร์คพอยท์", src: "/customer/workpoint.webp" },
         { name: "KFC", src: "/customer/kfc.webp" },
-        { name: "การท่าอากาศยานแห่งประเทศไทย", src: "/customer/aot.webp" },
         { name: "Toyota", src: "/customer/toyota.webp" },
-        { name: "ปูนซิเมนต์ไทย", src: "/customer/SCG.webp" },
+        { name: "ปูนซิเมนต์ไทย", src: "/customer/scg.webp" },
+        { name: "การท่าอากาศยานแห่งประเทศไทย", src: "/customer/aot.webp" },
       ],
     };
   },
