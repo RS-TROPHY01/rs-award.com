07be10dce932213a9562b92dd52c6685a49d662f
NooobtimeX <u2gstudio@gmail.com>
2023-12-21 14:06:31 +0700

    Update [slug].vue

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -1,40 +1,22 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
     <div>
-      <img src="/test/1600900.png" />
+      <img :src="product.img" />
     </div>
-    <div class="m-auto grid grid-cols-1">
-      <h1 class="text-center p-4">{{ product.name }}</h1>
-      <div class="flex">
-        <label class="mx-auto p-1" for="size">ไซน์:</label>
-        <select v-model="selectedSize" id="size" class="p-1 border-2">
-          <option
-            v-for="size in product.size"
-            :key="size.name"
-            :value="size.name"
-            :disabled="!size.inStock"
-          >
-            {{ size.name }}:{{ size.price }} บาท
-            {{ !size.inStock ? " หมด" : "" }}
-          </option>
-        </select>
-      </div>
-      <div class="m-auto flex">
-        <span class="p-1">จำนวน:</span>
-        <input
-          type="number"
-          v-model="quantity"
-          min="1"
-          class="w-12 text-center border-2"
-        />
-      </div>
-      <div class="p-1 m-auto">
-        <button
-          @click="addToCart(product, selectedSize, quantity)"
-          class="mx-auto text-xs m-auto"
-        >
-          เพิ่มลงตะกร้า
-        </button>
+    <div class="m-auto grid grid-cols-1 text-center">
+      <h1 class="p-4">{{ product.name }}</h1>
+      <div>
+        <div v-if="product.size[0].name == 'no size'">
+          <div>{{ product.size[0].price }} บาท</div>
+          <div class="grid grid-cols-3 gap-2">
+            <div>กว้าง: {{ product.size[0].w }}</div>
+            <div>ยาว: {{ product.size[0].l }}</div>
+            <div>สูง: {{ product.size[0].h }}</div>
+          </div>
+        </div>
+        <div v-else>
+          <div></div>
+        </div>
       </div>
     </div>
   </div>
@@ -47,39 +29,7 @@ export default {
   data() {
     return {
       products,
-      cart: [],
-      selectedSize: null,
-      quantity: 1,
-      product: {},
     };
-  },
-  methods: {
-    addToCart(product, selectedSize, quantity) {
-      if (process.client) {
-        if (!selectedSize) {
-          alert("กรุณาเลือกขนาด ก่อนเพิ่มสินค้าลงตะกร้า");
-          return;
-        }
-        const existingItem = this.cart.find(
-          (item) =>
-            item.name === product.name && item.size.name === selectedSize,
-        );
-        if (existingItem) {
-          existingItem.quantity += quantity;
-        } else {
-          const sizeInfo = product.size.find(
-            (size) => size.name === selectedSize,
-          );
-          const cartItem = {
-            name: product.name,
-            size: { name: selectedSize, price: sizeInfo.price },
-            quantity: quantity,
-          };
-          this.cart.push(cartItem);
-        }
-        localStorage.setItem("cart", JSON.stringify(this.cart));
-      }
-    },
   },
   created() {
     const slug = this.$route.params.slug;
