25100b0a9c5ce9def9d1d2b171c00b3b7d99aae0
NooobtimeX <u2gstudio@gmail.com>
2023-12-26 10:19:55 +0700

    update Crystal size

--- assets/productCrystal.ts
+++ assets/productCrystal.ts
@@ -1,71 +1,105 @@
 const productCrystal = [
   {
     name: "RS2302-3",
-    size: [{ name: "no size", price: 1800, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1800, inStock: true, w: 60, l: 80, h: 230 },
+    ],
   },
   {
     name: "RS2304-1",
-    size: [{ name: "no size", price: 1200, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1200, inStock: true, w: 50, l: 70, h: 275 },
+    ],
   },
   {
     name: "RS2314-2",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 40, l: 135, h: 205 },
+    ],
   },
   {
     name: "RS2320-1",
-    size: [{ name: "no size", price: 950, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 950, inStock: true, w: 80, l: 80, h: 280 },
+    ],
   },
   {
     name: "RS2340-3",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 56, l: 56, h: 200 },
+    ],
   },
   {
     name: "RS2345",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 140, l: 30, h: 70 },
+    ],
   },
   {
     name: "RS2350",
-    size: [{ name: "no size", price: 850, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 850, inStock: true, w: 35, l: 100, h: 220 },
+    ],
   },
   {
     name: "RS2357",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 130, l: 40, h: 250 },
+    ],
   },
   {
     name: "RS2360",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 54, l: 56, h: 200 },
+    ],
   },
   {
     name: "RS2362",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 60, l: 60, h: 230 },
+    ],
   },
   {
     name: "RS2363",
-    size: [{ name: "no size", price: 1100, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1100, inStock: true, w: 40, l: 105, h: 270 },
+    ],
   },
   {
     name: "RS2379",
-    size: [{ name: "no size", price: 1200, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1200, inStock: true, w: 60, l: 60, h: 200 },
+    ],
   },
   {
     name: "RS2393",
-    size: [{ name: "no size", price: 1400, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1400, inStock: true, w: 80, l: 50, h: 210 },
+    ],
   },
   {
     name: "RS2397-2",
-    size: [{ name: "no size", price: 1400, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1400, inStock: true, w: 50, l: 50, h: 180 },
+    ],
   },
   {
     name: "RS23127",
-    size: [{ name: "no size", price: 1400, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1400, inStock: true, w: 58, l: 58, h: 180 },
+    ],
   },
   {
     name: "RS23108",
-    size: [{ name: "no size", price: 950, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 950, inStock: true, w: 80, l: 35, h: 250 },
+    ],
   },
   {
     name: "RS23400",
-    size: [{ name: "no size", price: 1200, inStock: true, w: 1, l: 1, h: 1 }],
+    size: [
+      { name: "no size", price: 1200, inStock: true, w: 80, l: 80, h: 230 },
+    ],
   },
 ];
 
